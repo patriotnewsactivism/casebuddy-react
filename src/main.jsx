c2999344e452410e1ac642bbf8a20c174ba61a40
Matthew Reardon <patriotnewsactivism@proton.me>
2025-09-03 06:30:10 -0500

    Replace main.jsx to mount App with ReactDOM and wrap with Redux Provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,2 +1,14 @@
-﻿import "./style.css";
-import App from "./App.jsx";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './store';
+import './style.css';
+import App from './App.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
